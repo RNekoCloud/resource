14b443c55b70f4b1b64e2046bd4abdf7d4e62499
RageNeko26 <rneko2006@gmail.com>
2023-12-14 23:03:36 +0700

    feat: Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,14 @@ export default function Home() {
         * Also, it should listing all of available docs.
       */}
       <div className="container mt-3 mx-10">
-        <div className="w-1/2">
-          <h2 className="text-xl font-bold text-slate-400">What is this site about?</h2>
-          <p className="text-md my-3">Web ini adalah dokumentasi belajar terkait tentang <i>version control</i>, <i>programming</i>, <i>tools</i> dll</p>
+        <div className="w-3/4 mx-auto mt-20">
+          <h2 className="text-7xl font-bold text-slate-400 text-center">Practice Make It Perfect!</h2>
+          <p className="text-lg my-10 text-center">Butuh referensi? dokumentasi? ataupun sample kode? Sepertinya kamu berada di tempat yang tepat! <b>Resource</b> adalah dokumentasi <i>technical</i> yang berkaitan tentang <i>version control</i>, <i>programming</i>, <i>tools</i> dll</p>
+         <div className="flex justify-center">
+             <a href="#" className="px-12 py-3 rounded-lg bg-green-500 text-white text-center mx-5">Explore</a>
+             <a href="#" className="px-12 py-3 rounded-lg bg-green-500 text-white text-center">Source Code</a>
+
+         </div>
         </div>
       </div>
     </main>
