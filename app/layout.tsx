c2be31022a87fe13b630a11679e091f9c616538e
RageNeko26 <rneko2006@gmail.com>
2023-12-14 16:34:29 +0700

    feat: Applying navbar to global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from '@/components/Navbar/Navbar'
 
 export const metadata: Metadata = {
   title: 'Learning Resource',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="font-Poppins">{children}</body>
+      <body className="font-Poppins">
+        <Navbar></Navbar>
+        {children}
+        </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar/Navbar"
+
 export default function Home() {
   return (
     <main>
