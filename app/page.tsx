1ee4dc50238d449aa4bc4f32f1afbd52d65af31b
RageNeko26 <rneko2006@gmail.com>
2023-12-14 16:43:36 +0700

    docs: Add mini todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h2>Hello world</h2>
+      {/*
+        * Create content in here.
+        * It should contains  summary about this page.
+        * Also, it should listing all of available docs.
+      */}
     </main>
   )
 }
