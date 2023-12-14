62000799337f4b008ad0fbe7cf2a9aa1bb650711
RageNeko26 <rneko2006@gmail.com>
2023-12-14 16:52:40 +0700

    feat: Add base content in root

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
         * It should contains  summary about this page.
         * Also, it should listing all of available docs.
       */}
+      <div className="container mt-3 mx-10">
+        <div className="w-1/2">
+          <h2 className="text-xl font-bold text-slate-400">What is this site about?</h2>
+          <p className="text-md my-3">Web ini adalah dokumentasi belajar terkait tentang <i>version control</i>, <i>programming</i>, <i>tools</i> dll</p>
+        </div>
+      </div>
     </main>
   )
 }
