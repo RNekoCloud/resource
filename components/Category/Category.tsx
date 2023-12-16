a6eea644a68f66b767ce1caa0a7e97cc4c92172d
RageNeko26 <rneko2006@gmail.com>
2023-12-16 14:21:23 +0700

    feat: Add smooth scroll

--- components/Category/Category.tsx
+++ components/Category/Category.tsx
@@ -4,7 +4,7 @@ import git from "../../public/git.png"
 export default function Category() {
 
     return(
-        <div className="container mt-36">
+        <section className="container mt-36" id="category">
             <div className="w-3/4 mx-auto">
                 <h2 className="text-4xl font-semibold text-center">Category</h2>
             </div>
@@ -41,6 +41,6 @@ export default function Category() {
                 >
                 </Card>
             </div>
-        </div>
+        </section>
     )
 }
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
           <h2 className="text-7xl font-bold text-center">Practice Makes Perfect!</h2>
           <p className="text-lg my-10 text-center text-slate-600">Butuh referensi? dokumentasi? ataupun sample kode? Sepertinya kamu berada di tempat yang tepat! <b>Resource</b> adalah dokumentasi <i>technical</i> yang berkaitan tentang <i>version control</i>, <i>programming</i>, <i>tools</i> dll</p>
          <div className="flex justify-center">
-             <a href="#" className="px-12 py-3 rounded-lg bg-green-500 text-white text-center hover:opacity-50 transition-all ease-in-out">Let's Explore</a>
+             <a href="#category" className="px-12 py-3 rounded-lg bg-green-500 text-white text-center hover:opacity-50 transition-all ease-in-out">Let's Explore</a>
          </div>
         </div>
       </div>
